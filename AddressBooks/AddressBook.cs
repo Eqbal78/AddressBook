136a56132c64b4ce9713cc2f23fd85b4d1e1b231
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-08 13:56:29 +0530

    Welcome to Address Book Program

---
+++ AddressBooks/AddressBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBooks
+{
+    class AddressBook
+    { 
+        // Create a Constractor
+        public AddressBook()
+        {
+
+        }
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome to Address Book Program ");
+        }
+    }
+}
---
+++ AddressBooks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook obj = new AddressBook();
+            obj.Welcome();
+        }
+    }
+}
